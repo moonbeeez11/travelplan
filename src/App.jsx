c98a03b2bc6e5b869b123f8008efeb0321caec46
jeetenmndl <jeetenmndl96@gmail.com>
2025-08-19 15:36:09 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <button>HTML button</button>
+      <br />
+      <Button>Shadcn Button</Button>
+    </>
+  )
+}
+
+export default App
