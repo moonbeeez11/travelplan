9004749600a9f760068fb2e5e102ffd9254b7934
jeetenmndl <jeetenmndl96@gmail.com>
2025-08-27 21:19:06 +0545

    api working

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const  login  = useAuth();
+    const  {login}  = useAuth();
   const navigate = useNavigate();
 
 
@@ -17,8 +17,9 @@ const Login = () => {
             email: document.getElementById('email').value,
             password: document.getElementById('password').value,
         }
-      const { token } = await registerUser(form);
-      login({ email: form.email }, token);
+      const response = await registerUser(form);
+      console.log(response);
+    //   login({ email: form.email }, token);
       navigate("/dashboard");
     } catch (err) {
       console.error(err);
@@ -28,7 +29,7 @@ const Login = () => {
   return (
     <div>
         <form>
-        <input id='name' type="text" placeholder='username' />
+        <input id='name' type="text" placeholder='name' />
         <input id='email' type="email" placeholder='email' />
         <input id='password' type="password" placeholder='password' />
 
